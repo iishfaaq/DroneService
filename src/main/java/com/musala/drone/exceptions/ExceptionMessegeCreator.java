6138c99abbedd5adb01adad2f7c27d74eb5fc136
Ishfaaq Ismath <ishfaaqismath@social27.com>
2023-05-24 04:04:04 +0530

    Fixed Exception Message type Issues.

--- src/main/java/com/musala/drone/exceptions/ExceptionMessegeCreator.java
+++ src/main/java/com/musala/drone/exceptions/ExceptionMessegeCreator.java
@@ -1,5 +1,6 @@
 package com.musala.drone.exceptions;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class ExceptionMessegeCreator {
+	@Autowired
 	private  MessageSource messageSource;
 	
 	public String createMessage(String code) {
